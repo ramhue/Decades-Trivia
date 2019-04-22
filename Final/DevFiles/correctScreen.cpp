0d8dc302886158aa097063d2bb3f64f7695f59a7
ramhue <kbframiro@yahoo.com>
2019-04-22 08:59:19 -0700

    UPDATED
    UPDATED THE DESCRIPTION OF THE FUNCTIONS

--- Final/DevFiles/correctScreen.cpp
+++ Final/DevFiles/correctScreen.cpp
@@ -1,11 +1,20 @@
 #include "stdafx.h"
 #include "correctScreen.h"
 
+/*********************************************************
+NAME:corectScreen::Show
+DESCRIPTION: THIS FUNCTION LOADS FONT AND ADJUSTS THE 
+			 SCREEN TO SHOW THE TEXT CORRECT AND PLACES
+			 IT IN THE CORRECT POSITION.THEN IT LOOPS 
+			 AND POLLS THE EVENT TO SEE IF THE USE MOUSE 
+			 CLICKED,KEYPRESSED, OR IF THE USER CLOSED THE 
+			 WINDOW. IF THE EVENT IS TRUE, THEN THE FUNCTION 
+			 RETURNS AND THE USER GOES BACK TO THE PLAYING 
+			 SCREEN 
+************************************************************/
+
 void correctScreen::Show(sf::RenderWindow& window) {
-	/*sf::Texture splash;
-	if (!(splash.loadFromFile("images/image.png"))) {
-		return;
-	}*/
+
 	sf::Font myFont;
 	myFont.loadFromFile("BillionaireMediumGrunge.ttf");
 	sf::Text MyText("CORRECT!", myFont, 200);
@@ -14,7 +23,6 @@ void correctScreen::Show(sf::RenderWindow& window) {
 	float text_posY = (768 - MyText.getLocalBounds().height) / 2;
 	sf::Vector2f text_pos(text_posX, text_posX);
 	MyText.setPosition(text_pos);
-	//sf::Sprite sprite(splash);
 
 	sf::Event event;
 	while (window.isOpen()) {
@@ -22,13 +30,11 @@ void correctScreen::Show(sf::RenderWindow& window) {
 			if (event.type == sf::Event::EventType::KeyPressed ||
 				event.type == sf::Event::EventType::MouseButtonPressed ||
 				event.type == sf::Event::EventType::Closed) {
-				//renderWindow.close();
-				
+\				
 				return;
 			}
 		}
 		window.clear(sf::Color(50, 205, 50,255));
-		//window.draw(sprite);
 		window.draw(MyText);
 		window.display();
 	}
